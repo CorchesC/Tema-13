39a6f7fc1d465edd58938e72ec226e3d238bab88
Corches Ciprian <cipriancorches@gmail.com>
2023-02-10 15:37:36 +0200

    tema-13

---
+++ Tema-13/functii.h
@@ -0,0 +1,258 @@
+#pragma once
+#pragma once
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void citire(int v[], int& dim) {
+
+	ifstream f("numere.txt");
+
+	f >> dim;
+	for (int i = 0; i < dim; i++) {
+
+		f >> v[i];
+	}
+
+	f.close();
+}
+void afisare(int v[], int dim) {
+	for (int i = 0; i < dim; i++) {
+		cout << v[i] << " ";
+	}
+	cout << endl;
+
+}
+void secventiereElementeIntreExtEgale(int v[], int dim) {
+    int dr = 0;
+    int stg = 0;
+    for (int i = 1; i <= dim; ++i)
+    {
+        for (int j = i + 1; j <= dim; ++j)
+        {
+            if (v[i] == v[j])
+            {
+
+                stg = i;
+                dr = j;
+
+            }
+        }
+    }
+
+    for (int f = 0; f <= dim; f++) {
+        if (f >= stg && f <= dr) {
+            cout << v[f] << " ";
+        }
+    }
+}
+void secventierePanta(int v[], int dim) {
+	int smax = 1;
+	int dmax = 0;
+	for (int i = 0; i < dim; i++) {
+		if (v[i] == v[i-1] +1) {
+			int j = i;
+			while (j + 1 < dim && v[j] == v[j - 1] + 1) {
+				j++;
+			}
+			if (j - i + 1 > dmax - smax + 1) {
+				smax = i, dmax = j;
+
+			}
+
+		}
+	}
+	for (int f = 0; f <= dim; f++) {
+		if (f >= smax && f <= dmax) {
+			cout << v[f] << " ";
+		}
+	}
+	cout << endl;
+}
+void secventiereSol3(int v[], int dim,int n) {
+	int smax = 1;
+	int dmax = 0;
+	for (int i = 0; i < dim; i++) {
+		for (int j = i + 1; j < dim; ++j) {
+			int s = 0;
+			for (int f = i; f < j; f++) {
+				s += v[f];
+			}
+			if (s != 0 && s % n == 0 && j - i + 1 > dmax - smax + 1) {
+				smax = i; dmax = j;
+			}
+		}
+	}
+	for (int f = 0; f <= dim; f++) {
+		if (f >= smax && f <= dmax) {
+			cout << v[f] << " ";
+		}
+	}
+	cout << endl;
+}
+int nrCifreDistincte(int n) {
+	int nrc = 0;
+	while (n != 0) {
+		int cifra = n % 10;
+		int nr = n;
+		int x = 0;
+		for (int i = 0; nr > 0; nr = nr / 10) {
+			i = nr % 10;
+			if (i == cifra) {
+				x++;
+			}
+		}
+		if (x <= 1) {
+			nrc++;
+		}
+		n = n / 10;
+	}
+	return nrc;
+}
+void secventiereNrcDiferite(int v[], int dim) {
+	int smax = 1;
+	int dmax = 0;
+	for (int i = 0; i < dim; i++) {
+		if (nrCifreDistincte(v[i]) == nrCifreDistincte(v[i + 1])) {
+			int j = i;
+			while (j + 1 < dim && nrCifreDistincte(v[j]) == nrCifreDistincte(v[j + 1])) {
+				j++;
+			}
+			if (j - i + 1 > dmax - smax + 1) {
+				smax = i, dmax = j;
+
+			}
+
+		}
+	}
+	for (int f = 0; f <= dim; f++) {
+		if (f >= smax && f <= dmax) {
+			cout << v[f] << " ";
+		}
+	}
+	cout << endl;
+}
+void citire2(int v[], int& dim) {
+
+	ifstream f("numere2.txt");
+
+	f >> dim;
+	for (int i = 0; i < dim; i++) {
+
+		f >> v[i];
+	}
+
+	f.close();
+}
+void secventaVectorInclus(int v[], int dim, int v2[], int dim2) {
+	int stg = 0;
+	int dr = 0;
+	for (int i = 0; i < dim; i++) {
+		if (v[i] = v2[i]) {
+			for (int f = 0; f < dim2; f++) {
+				if (v[f] == v2[f]) {
+					stg = i;
+					dr = f;
+				}
+			}
+		}
+		
+	}
+	cout << stg << " " << dr;
+} 
+bool paritateDiferita(int n, int m) {
+	cout << n << endl;
+	if (n % 2 == 0) {
+		if (m % 2 != 0) {
+			return true;
+		}
+		else if (m % 2 == 0) {
+			return false;
+		}
+	}
+	if (n % 2 != 0) {
+		if (m % 2 == 0) {
+			return true;
+		}
+		else if (m % 2 != 0) {
+			return false;
+		}
+	}
+}
+void secventaParitatiDiferite(int v[], int dim) {
+	int smax = 1;
+	int dmax = 0;
+	for (int i = 0; i < dim; i++) {
+		if (v[i] < v[i + 1] && paritateDiferita(v[i], v[i + 1]) == true) {
+			int j = i;
+			while (j + 1 < dim && v[j] < v[j + 1] && paritateDiferita(v[j], v[j + 1]) == true) {
+				j++;
+			}
+			if (j - i + 1 > dmax - smax + 1) {
+				smax = i, dmax = j;
+
+			}
+		}
+	}
+	cout << smax << " " << dmax << endl;
+}
+int sumaSecvente(int v[], int dim) {
+	int s = 0;
+	for (int i = 0; i < dim; i++) {
+		for (int x = i; x < dim; x++) {
+			s += v[x];
+		}
+	}
+	return s;
+}
+int elementMaximPoz(int v[], int dim) {
+	int eMax = -1;
+	int poz = 0;
+	for (int i = 0; i < dim; i++) {
+		if (v[i] > eMax) {
+			eMax = v[i];
+			poz = i;
+		}
+	}
+	return poz;
+}
+int elementMinimPoz(int v[], int dim) {
+	int eMin = 9999;
+	int poz = 0;
+	for (int i = 0; i < dim; i++) {
+		if (v[i] < eMin) {
+			eMin = v[i];
+			poz = i;
+		}
+	}
+	return poz;
+}
+int sumaIntreMaxMin(int v[], int dim) {
+	int max = elementMaximPoz(v, dim);
+	int min = elementMinimPoz(v, dim);
+	int s = 0;
+	for (int i = 0; i < dim; i++) {
+		if (i >= min && i <= max) {
+			s += v[i];
+		}
+	}
+	return s;
+}
+void sumaSecventeSelectate(int v[], int dim,int n) {
+	int ctr = 0;
+	while (ctr != n) {
+		cout << "Introduceti i: " << endl;
+		int i;
+		cin >> i;
+		cout << " Introduecti j: " << endl;
+		int j = 0;
+		cin >> j;
+		int s = 0;
+		for (int f = 0; f < dim; f++) {
+			if (i >= i && i <= j) {
+				s += v[i];
+			}
+		}
+		cout << s << endl;
+	}
+}
---
+++ Tema-13/solutii.h
@@ -0,0 +1,71 @@
+﻿#pragma once
+#include "functii.h"
+using namespace std;
+
+void sol1() {
+	//Se dă un şir de numere. Găsiți un subşir de lungime maximă care începe și se sfârşeşte cu acelaşi număr.
+	int v[100], d = 0;
+	citire(v, d);
+	secventiereElementeIntreExtEgale(v, d);
+}
+void sol2() {
+	//Definim noțiunea de pantă ca fiind o secvență de elemente consecutive din vectorul A astfel încât a[i] = a[i - 1] + 1. Să se determine panta de lungimea maximă.
+	int v[100], d = 0;
+	citire(v, d);
+	secventierePanta(v, d);
+}
+void sol3() {
+	//Pentru un vector x de n numere întregi dat, să se afişeze o secvenţă (dacă există) de elemente consecutive din vector pentru care suma elementelor se divide cu n.
+	int v[100], d = 0;
+	citire(v, d);
+	cout << "Introduceti n: " << endl;
+	int n;
+	cin >> n;
+	secventiereSol3(v, d,n);
+}
+void sol4() {
+	//Se consideră un şir de n numere naturale mai mici decât 32000. Se cere să se determine o succesiune cu număr maxim de elemente din şir, care are proprietatea că fiecare componentă a succesiunii are acelaşi număr de cifre distincte în scrierea sa.
+	int v[100], d = 0;
+	citire(v, d);
+	secventiereNrcDiferite(v, d);
+}
+void sol5() {
+	//Se dau doi vectori A cu m componente şi B cu n componente (n>m). Componentele ce formează vectorul A sunt distincte două câte două.Să se scrie un program care să verifice dacă toate componentele vectorului A luate în orice ordine se găsesc în vectorul в pe poziții consecutive.Dacă testul este reuşit să se afişeze fiecare poziție din vectorul B unde începe secvenţa de elemente din A.
+	int v[100], d = 0;
+	citire(v, d);
+	int n[100], k = 0;
+	citire2(n, k);
+	secventaVectorInclus(v, d,n,k);
+}
+void sol6() {
+	//Se dă un şir cu n elemente, numere naturale. Determinaţi cea mai lungă secvenţă de elemente din şir cu proprietatea că oricare două valori consecutive în secvenţă au parităţi diferite.
+	int v[100], d = 0;
+	citire(v, d);
+	secventaParitatiDiferite(v, d);
+}
+void sol7() {
+	//Se consideră un vector cu n elemente numere naturale. Calculați suma sumelor tuturor subsecvențelor ce se pot forma cu elementele vectorului. Pentru că suma poate fi foarte mare, afișați suma modulo 1.000.000.007.
+	int v[100], d = 0;
+	citire(v, d);
+	int s = sumaSecvente(v, d);
+	cout << "Suma tuturor secventelor vectorului este: " << s << endl;
+}
+void sol8() {
+	//Se dă un şir format din n numere naturale distincte. Calculați suma elementelor din secvenţa ce uneşte cel mai mic şi cel mai mare element din şir.
+	int v[100], d = 0;
+	citire(v, d);
+	int s = sumaIntreMaxMin(v, d);
+	cout << "Suma elementelor formate din secventa cu extermintatiile elementul maxim si cel mimim al vectorului este: " << endl;
+}
+void sol9() {
+	//Fie N un numar natural și un șir de N numere naturale V[1], V[2], …, V[N]. Pentru M întrebări de forma (i,j), să se calculeze suma termenilor V[i], V[i + 1], …, V[j].
+	int v[100], d = 0;
+	citire(v, d);
+	cout << "cate secvente doriti sa introudceti: " << endl;
+	int n;
+	cin >> n;
+	sumaSecventeSelectate(v, d, n);
+}
+void sol10() {
+	//
+}
